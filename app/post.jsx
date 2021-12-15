5d2da0e810ed489f69614f372852da3724cab162
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-12-15 12:54:51 -0500

    enable getPost func to read from file system

--- app/post.jsx
+++ app/post.jsx
@@ -1,13 +1,20 @@
-export function getPosts() {
-  const posts = [
-    {
-      slug: "my-first-post",
-      title: "My First Post"
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I made Just for Me"
-    }
-  ];
-  return posts;
+import path from "path";
+import fs from "fs/promises";
+import parseFrontMatter from "front-matter";
+
+const postsPath = path.join(__dirname, "..", "posts")
+
+export async function getPosts() {
+  const dir = await fs.readdir(postsPath);
+  return Promise.all(
+    dir.map(async filename => {
+      const file = await fs.readFile(path.join(postsPath, filename));
+
+      const { attributes } = parseFrontMatter(file.toString());
+      return {
+        slug: filename.replace(/\.md$/, ""),
+        title: attributes.title
+      };
+    })
+  );
 }
