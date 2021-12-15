628654515792d1ee86eda30d3b27eae4bf3d113d
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-12-15 13:29:03 -0500

    add loader to access params

--- app/routes/posts/$slug.jsx
+++ app/routes/posts/$slug.jsx
@@ -1,8 +1,15 @@
+import { useLoaderData } from "remix";
+
+export const loader = async ({ params }) => {
+  return params.slug;
+}
 
 export default function PostSlug() {
+  const slug = useLoaderData();
+  console.log("here")
   return (
     <div>
-      <h1>Some Post</h1>
+      <h1>Some Post: {slug}</h1>
     </div>
   );
 }
