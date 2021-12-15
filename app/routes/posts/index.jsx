c03bd1039eeb0dde9b37d63e0a94ecff38fcadc6
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-12-15 11:57:01 -0500

    implement posts route

--- app/routes/posts/index.jsx
+++ app/routes/posts/index.jsx
@@ -1,7 +1,31 @@
+import { Link, useLoaderData } from "remix";
+
+export const loader = () => {
+  return [
+    {
+      slug: "my-first-post",
+      title: "My First Post"
+    },
+    {
+      slug: "90s-mixtape",
+      title: "A Mixtape I made Just for Me"
+    }
+  ];
+};
+
 export default function Post() {
+  const posts = useLoaderData();
+  console.log(posts);
   return (
     <div>
       <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
