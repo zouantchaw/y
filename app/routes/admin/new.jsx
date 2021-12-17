69871a24beeb733d869cd8d70ad3b2608dd17f57
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-12-17 13:46:51 -0500

    add form to new admin route

--- app/routes/admin/new.jsx
+++ app/routes/admin/new.jsx
@@ -1,3 +1,26 @@
+import { Form } from "remix"
+
 export default function NewPost() {
-  return <h2>New Post</h2>
+  return(
+    <Form method="post">
+      <p>
+        <label>
+          Post Title: <input type="text" name="title" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Post Slug: <input type="text" name="slug" />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="markdown">Markdown:</label>
+        <br />
+        <textarea id="markdown" rows={20} name="markdown" />
+      </p>
+      <p>
+        <button type="submit">Create Post</button>
+      </p>
+    </Form>
+  )
 }
