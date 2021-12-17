961bc17cc781be987886b6e57c352ba5c808584a
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-12-17 14:21:16 -0500

    createPost func and form action

--- app/routes/admin/new.jsx
+++ app/routes/admin/new.jsx
@@ -1,4 +1,17 @@
-import { Form } from "remix"
+import { Form, redirect } from "remix"
+import { createPost } from "../../post"
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  await createPost({ title, slug, markdown });
+
+  return redirect("/admin")
+}
 
 export default function NewPost() {
   return(
---
+++ posts/ksdjnfdln.md
@@ -0,0 +1,5 @@
+---
+title: kjdabkajn
+---
+
+dsncldknldkna
